c4ae37b45000e7ed2ad3180ff59a06de0af79110
Alex Chung <awchung1357@gmail.com>
2026-07-01 21:54:35 -0400

    Added abbvreation

--- route_constants.py
+++ route_constants.py
@@ -2,7 +2,7 @@ ROUTES = {
     "7": {
         "color": (154, 56, 161),  # MTA purple
         "uptown": "Flushing-Main St",
-        "downtown": "34 St-Hudson Yards",
+        "downtown": "34 St-Hudson Yds",
         "logo": "assets/route-7-logo.png",
     }
 }
